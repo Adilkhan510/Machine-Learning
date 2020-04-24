aa8fa16b0065865ecd161f5ca505b84519ac9480
adilkhan <adilkhan@ucsb.edu>
2020-04-23 18:34:56 -0700

    Added some comments to the code

--- test.py
+++ test.py
@@ -14,9 +14,14 @@ print(data)
 predict = "G3"
 X = np.array(data.drop([predict],1))
 Y = np.array(data[predict])
+## splitting the data. 10% for testing and 90% for training.
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
 
+#importing linear regression from sklearn so we can train our data model to use
 linear = linear_model.LinearRegression()
+# calling the fit function to train. The x-axis represents the categories we are trying to use to predict with y the final score
+
 linear.fit(x_train,y_train)
+#test the model against the test data we saved earlier.
 accuracy = linear.score(x_test, y_test)
 print(accuracy)
